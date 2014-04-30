bda92392d2b1d953f21c61ee08fdf00d7169263d
givanse <silvag777@gmail.com>
2014-04-30 08:09:08 -0500

    copy bootstrap font files

--- scripts/install-bootstrap.sh
+++ scripts/install-bootstrap.sh
@@ -6,8 +6,8 @@ bootstrapZip='bootstrap-3.0.0-dist.zip'
 bootstrapFolder='bootstrap-3.0.0'
 
 # cleanup
-rm $bootstrapZip
-rm -r $bootstrapFolder
+rm $bootstrapZip || true
+rm -r $bootstrapFolder || true
 
 # http://getbootstrap.com/
 wget https://github.com/twbs/bootstrap/releases/download/v3.0.0/bootstrap-3.0.0-dist.zip
@@ -21,6 +21,12 @@ destiny='www/css/'
 cp -v $origin'bootstrap.css' $destiny
 cp -v $origin'bootstrap.min.css' $destiny
 
+# fonts
+
+origin=$bootstrapFolder'/dist/fonts/*'
+destiny='www/fonts/'
+cp -v $origin $destiny
+
 # js
 
 origin=$bootstrapFolder'/dist/js/'
@@ -31,6 +37,8 @@ cp -v $origin'bootstrap.min.js' $destiny
 # cleanup
 rm $bootstrapZip
 rm -r $bootstrapFolder
+
+echo 'Done!'
 
 exit
 #EOF
