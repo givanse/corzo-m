9a403a09f7a176cea1af5539041c127f075dd027
givanse <silvag777@gmail.com>
2014-04-29 20:41:04 -0500

    add basic dummy templates

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,9 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+    this.resource("service_details");
+    this.resource("my_account");
+    this.resource("preferences");
 });
 
 App.IndexRoute = Ember.Route.extend({
@@ -9,3 +11,21 @@ App.IndexRoute = Ember.Route.extend({
     return ['red', 'yellow', 'blue'];
   }
 });
+
+App.ServiceDetailsView = Ember.View.extend({
+    templateName: 'servicedetails'
+});
+
+App.MyAccountView = Ember.View.extend({
+    templateName: 'my_account'
+});
+
+App.PreferencesView = Ember.View.extend({
+    templateName: 'preferences'
+});
+
+App.ContextMenuComponent = Ember.Component.extend({
+  classNames: ['row', 'contextmenu']
+});
+
+/*EOF*/
