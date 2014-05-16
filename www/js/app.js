636e4783b105ce26c206cb341f37c7f5c9729fec
givanse <silvag777@gmail.com>
2014-05-16 08:27:38 -0500

    IndexController shared by IndexRoute and MonitorRoute

--- www/js/app.js
+++ www/js/app.js
@@ -5,102 +5,199 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-    this.route("service_details");
-    this.route("my_account");
+    this.route("account");
+    this.route("current_task");
+    this.route("monitor");
     this.route("preferences");
 });
 
-var dummyUser = {
-    name: 'Juan Camaney',
-    cellphone: '33 1234 0000',
-    license: '987654321',
-    plate: 'abc 123'
-};
+/* Routes */
 
-App.MyAccountRoute = Ember.Route.extend({
+App.AccountRoute = Ember.Route.extend({
     model: function() {
-        return dummyUser;
+        return fixtureDummyUser;
     }
+});
+
+App.IndexRoute = Ember.Route.extend({
+    model: function() {
+        return fixtureDefaultModel;
+    }
+});
+
+App.MonitorRoute = Ember.Route.extend({
+    /* This allow us to share the same model with the MapRoute. */
+    controllerName: 'index'
 });
 
 /* Controllers */
 
-App.MyAccountController = Ember.Controller.extend({
-    geopos: {
-        timestamp: 0, 
-        coords: {
-            accuracy: 0,
-            altitude: 0,
-            altitudeAccuracy: 0,
-            heading: 'NA',
-            latitude: 90.000000,
-            longitude: 0.000000,
-            speed: 0 
-        }
+App.IndexController = Ember.Controller.extend({
+    init: function () { 
+        this._super();
+
+        /* watch for GPS location updates periodically */
+        var self = this;
+
+        var geolocationError = function (error) {
+            self.set('gpserror', error);
+        };
+
+        var successWrapper = function (position) {
+            self.geolocationSuccess(position);
+        };
+
+        var options = 
+            { maximumAge: 1000, timeout: 3000, enableHighAccuracy: true };
+        var watchPositionId = navigator.geolocation.watchPosition(successWrapper, 
+                                            geolocationError, 
+                                            options);
     },
 
+    initGoogleMap: function () {
+        var mapOptions = {
+            center: new google.maps.LatLng(20.674226, -103.387363),
+            zoom: 16,
+            maxZoom: 18,
+            disableDefaultUI: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            zoomControl: false
+        };
+        var mapcanvasElem = document.getElementById("map-canvas");
+        if ( ! mapcanvasElem ) {
+            console.log('[error] MapController.initGoogleMap:' +
+                        'There is no DOM element #map-canvas.');
+        }
+        var map = new google.maps.Map(mapcanvasElem, mapOptions);
+        this.set('map', map);
+
+        /* Add device marker */
+
+        /*http://code.google.com/p/google-maps-utility-library-v3/wiki/Libraries*/
+        //TODO: Remove library and use custom marker (if only this is being used)
+        var GeoMarker = new GeolocationMarker();
+        GeoMarker.setCircleOptions({fillColor: '#808080'});
+        GeoMarker.setMap( this.get('map') );
+    }, /* initGoogleMap */
+
+    geolocationSuccess: function (position) {
+
+        // Update the map
+        if ( this.get('map') ) {
+            var latlng = new google.maps.LatLng(position.coords.latitude,
+                                                position.coords.longitude);
+            this.get('map').setCenter( latlng );
+        } else {
+            console.log('[error] IndexController.geolocationSuccess:' +
+                        'There is no map instance.');
+        }
+
+        function uploadPosition() {
+            //TODO: upload position to the server
+        }
+
+        uploadPosition(position);
+
+        var formattedGeoPos = {
+            timestamp: '0 / 0 / 0 - 00:00',
+            coords: {
+                accuracy: '0',
+                altitude: '0.0',
+                altitudeAccuracy: '0',
+                heading: '0.0',
+                latitude: '0.000000',
+                longitude: '0.000000',
+                speed: '0'
+            }
+        };
+
+        var c = position.coords;                           /* Just a shurtcut */
+        var fc = formattedGeoPos.coords;                   /* Just a shurtcut */
+
+        fc.accuracy = ( c.accuracy ? c.accuracy : fc.accuracy  ) + ' m'; 
+        fc.altitude = ( c.altitude ? 
+                        c.altitude.toFixed(2) : fc.altitude ) + ' m'; 
+        fc.altitudeAccuracy = ( c.altitudeAccuracy ? 
+                                c.altitudeAccuracy : 
+                                fc.altitudeAccuracy  ) + ' m'; 
+        fc.heading = ( c.heading ? c.heading.toFixed(2) : fc.heading  ); 
+        fc.latitude = c.latitude ? c.latitude.toFixed(6) : fc.latitude ; 
+        fc.longitude = c.longitude ? c.longitude.toFixed(6) : fc.longitude ; 
+        if ( c.speed ) {
+            var ms = c.speed.toFixed(2);
+            var kmhr = (ms * 3600) / 1000;
+            fc.speed = ms + ' m/s (' + kmhr + ' km/hr)'; 
+        } else {
+            fc.speed = fc.speed + ' m/s';   
+        }
+
+        var time = new Date( position.timestamp );
+        var options = {
+            weekday: "long", year: "numeric", month: "short",
+            day: "numeric", hour: "2-digit", minute: "2-digit"
+        };
+        var date = time.toLocaleTimeString("es-mx", options);
+        this.set('timestamp', date);
+        this.set('coords', formattedGeoPos.coords);
+    }, /* geolocationSuccess */
+
+    map: null,                                        /* google map reference */
+
+    message: function () {
+        var err = this.get('model.gpserror');
+        if ( err && err.code ) {
+            return err.message + ', code: ' + err.code;
+        } else {
+            return 'ningun error';
+        }
+    }.property('gpserror')
+
+}); /* IndexController */
+
+/* Views */
+
+App.ApplicationView = Ember.View.extend({
+    classNames: ['ember-fulls-view']
+});
+
+App.MonitorView = App.IndexView = Ember.View.extend({
+    classNames: ['ember-fulls-view'],
+    didInsertElement: function() {
+        /* There are two #map-canvas, one in each view, but both views share
+           the same controller (IndexController). So, each time we swap views
+           a new google map is created and the old one overwritten. 
+           TODO: Review the performance toll. */
+        this.get('controller').initGoogleMap();
+    }
+});
+
+/* Fixtures */
+
+var fixtureDefaultModel = {
+    timestamp: 0, 
+    coords: {
+        accuracy: 0,                                                     /* m */
+        altitude: 0,                                                     /* m */
+        altitudeAccuracy: 0,                                             /* m */
+        heading: 'NA',        /* clockwise degrees relative to the true north */
+        latitude: 90.000000,                               /* decimal degrees */
+        longitude: 0.000000,                               /* decimal degrees */
+        speed: 0                                                       /* m/s */ 
+    },
     gpserror: {
-        message: "ningun error",
+        message: null,
         code: null,
         PERMISSION_DENIED: null, 
         POSITION_UNAVAILABLE: null, 
         TIMEOUT: null
-    },
-
-    init: function() { 
-        this._super();
-        var self = this;
-        var geolocationSuccess = function (position) {
-            self.set('geopos', position);
-            self.set('gpserror', null);
-        };
-        var geolocationError = function (error) {
-            self.set('gpserror', error);
-        };
-        navigator.geolocation.watchPosition(geolocationSuccess,
-                                            geolocationError,
-                                            {maximumAge: 1000, 
-                                             timeout: 2000, 
-                                             enableHighAccuracy: true});
-        
-    },
-
-    message: function () {
-        var err = this.get('gpserror');
-        return ( err && err.code ) ?
-                 'Error: ' + err.code + ', ' + err.message : ''; 
-    }.property('gpserror')
-});
-
-/* Views */
-
-App.IndexView = Ember.View.extend({
-    didInsertElement: function() {
-        App.hideContextMenu();
     }
-});
+};
 
-App.ServiceDetailsView = Ember.View.extend({
-});
-
-App.MyAccountView = Ember.View.extend({
-    didInsertElement: function() {
-        App.hideContextMenu();
-    }
-});
-
-App.PreferencesView = Ember.View.extend({
-    didInsertElement: function() {
-        App.hideContextMenu();
-    }
-});
-
-App.ContextMenuComponent = Ember.Component.extend({
-});
-
-App.hideContextMenu = function () {
-    //$("div#contextmenu").hide();
-    //$("div#contextmenu div:first-child").animate({width: 'toggle'});
+var fixtureDummyUser = {
+    name: 'Juan Camaney',
+    cellphone: '33 1234 0000',
+    license: '987654321',
+    plate: 'abc 123'
 };
 
 /*EOF*/
--- www/js/index.js
+++ www/js/index.js
@@ -1,35 +1,33 @@
 /**
+ *
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
+
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        //document.addEventListener('deviceready', this.onDeviceReady, false);
-        this.onDeviceReady();
-        $(document).ready(function() {
-            var ctxMenu$ = $("div#contextmenu");
-
-            $("#contextmenu-button").click(function () { 
-                ctxMenu$.animate({width: 'toggle'});
-            });
-            ctxMenu$.animate({width: 'toggle'});
-
-            $("#contextmenu ul li").click(function () { 
-                ctxMenu$.animate({width: 'toggle'});
-            });
-        });
+    bindEvents: function () {
+        // Phone
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        // Desktop
+        //$(document).ready(function() { app.onDeviceReady(); });
     },
-    // deviceready Event Handler
-    //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
+        var ctxMenu$ = $("#contextmenu");
+
+        $("#button-wrapper").click(function () { 
+            ctxMenu$.animate({width: 'toggle'});
+        });
+        ctxMenu$.animate({width: 'toggle'});
+
+        $("#contextmenu ul li").click(function () { 
+            ctxMenu$.animate({width: 'toggle'});
+        });
     }
 };
 
